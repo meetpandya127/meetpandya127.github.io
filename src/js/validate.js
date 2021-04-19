ec9969c22cae6edfc30bea426da1266f2625a003
meetpandya127 <mpandya6@myseneca.ca>
2021-04-18 21:37:22 -0400

    Final Changes Meet Pandya

--- src/js/validate.js
+++ src/js/validate.js
@@ -13,58 +13,28 @@ function validate(event) {
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
   console.log('TODO - validate the longitude, latitude values before submitting');
-  var lat = document.querySelector("#latitude").value;
-  var lon = document.querySelector("#longitude").value;
-
+  var latitude = document.querySelector("#latitude").value;
+  var longitude = document.querySelector("#longitude").value;
   var errorLatitude = document.getElementById("latitudeDiv");
   var errorLongitude = document.getElementById("longitudeDiv");
-
   if (errorLatitude.childNodes.length > 1) {
     errorLatitude.removeChild(errorLatitude.lastChild);
   }
-  
   if (errorLongitude.childNodes.length > 1) {
     errorLongitude.removeChild(errorLongitude.lastChild);
   }
-
-  if(lat) {
-    if (!isNaN(lat)) {
-      var latNum = parseFloat(lat);
-      if (latNum >= -90 && latNum <= 90) {
-        if(lon) {
-          if (!isNaN(lon)) {
-            var lonNum = parseFloat(lon);
-            if (lonNum >= -180 && lonNum <= 180) {
-              return true;      
-            }
-            else{
-              var txt = document.createTextNode(" must be a valid Longitude (-180 to 180)");
-              errorLongitude.appendChild(txt);
-              return false;
-            }
-          }
-          else{
-            var txt = document.createTextNode(" must be a valid Longitude (-180 to 180)");
-            errorLongitude.appendChild(txt);
-            return false;
-          }
-        }            
-      }
-      else{
-        var txt = document.createTextNode(" must be a valid Latitude (-90 to 90)");
-        errorLatitude.appendChild(txt);
-        return false;
-      }
-    }
-    else{
-      var txt = document.createTextNode(" must be a valid Latitude (-90 to 90)");
-      errorLatitude.appendChild(txt);
-      return false;
-    }
+  if(isNaN(latitude) || latitude < -90 || latitude > 90) {
+    var txt = document.createTextNode(" must be a valid Latitude (-90 to 90)");
+    errorLatitude.appendChild(txt);
+    return false;
   }
-
-  
-
+  else if(isNaN(longitude) || longitude < -180 || longitude > 180){
+    var txt = document.createTextNode(" must be a valid Longitude (-180 to 180)");
+    errorLongitude.appendChild(txt);
+    return false;
+  }
+  else 
+    return true;
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
